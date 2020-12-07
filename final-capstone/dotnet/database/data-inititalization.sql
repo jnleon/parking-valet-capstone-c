7d1f4b3ac22bb132b255899fd1e604813e46d78a
Brian LeMaster <brian.lemaster@outlook.com>
2020-12-07 10:20:39 -0500

    changes to db init script

--- final-capstone/dotnet/database/data-inititalization.sql
+++ final-capstone/dotnet/database/data-inititalization.sql
@@ -8,15 +8,15 @@ INSERT INTO users (username, password_hash, salt, user_role) VALUES ('user','Jg4
 INSERT INTO users (username, password_hash, salt, user_role) VALUES ('admin','YhyGVQ+Ch69n4JMBncM4lNF/i9s=', 'Ar/aB2thQTI=','admin');
 
 INSERT INTO parking_spots (is_occupied) VALUES (0);
-INSERT INTO parking_spots (is_occupied) VALUES (1);
 INSERT INTO parking_spots (is_occupied) VALUES (0);
-INSERT INTO parking_spots (is_occupied) VALUES (1);
 INSERT INTO parking_spots (is_occupied) VALUES (0);
-INSERT INTO parking_spots (is_occupied) VALUES (1);
 INSERT INTO parking_spots (is_occupied) VALUES (0);
-INSERT INTO parking_spots (is_occupied) VALUES (1);
 INSERT INTO parking_spots (is_occupied) VALUES (0);
-INSERT INTO parking_spots (is_occupied) VALUES (1);
+INSERT INTO parking_spots (is_occupied) VALUES (0);
+INSERT INTO parking_spots (is_occupied) VALUES (0);
+INSERT INTO parking_spots (is_occupied) VALUES (0);
+INSERT INTO parking_spots (is_occupied) VALUES (0);
+INSERT INTO parking_spots (is_occupied) VALUES (0);
 
 INSERT INTO parking_statuses (parking_status) VALUES ('Spot Requested');
 INSERT INTO parking_statuses (parking_status) VALUES ('Parked');
