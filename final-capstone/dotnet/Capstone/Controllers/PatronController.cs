de2fa962ee762fd7a9aae1d063cf4047ffd2828a
Brian LeMaster <brian.lemaster@outlook.com>
2020-12-08 11:05:25 -0500

    DAO, controller, and model changes for addl user/patron info

---
+++ final-capstone/dotnet/Capstone/Controllers/PatronController.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Capstone.DAO;
+using Capstone.Models;
+using Capstone.Security;
+using System.Collections.Generic;
+using Microsoft.AspNetCore.Authorization;
+
+namespace Capstone.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class PatronController : ControllerBase
+    {
+        private readonly IPatronDAO patronDAO;
+
+        public PatronController(IPatronDAO _patronDAO)
+        {
+            patronDAO = _patronDAO;
+        }
+
+        // https://localhost:44315/parkingspot/1
+        [HttpGet("{id}")]
+        public IActionResult GetByUserId(int id)
+        {
+            Patron p = patronDAO.GetByUserId(id);
+
+            if (p == null)
+            {
+                //return StatusCode(500);
+                return NoContent();
+            }
+            else
+            {
+                // Switch to 200 OK
+                return Ok(p);
+            }
+        }
+    }
+}
--- final-capstone/dotnet/Capstone/Models/User.cs
+++ final-capstone/dotnet/Capstone/Models/User.cs
@@ -46,5 +46,9 @@
         public string Password { get; set; }
         public string ConfirmPassword { get; set; }
         public string Role { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string EmailAddress { get; set; }
+        public string PhoneNumber { get; set; }
     }
 }
